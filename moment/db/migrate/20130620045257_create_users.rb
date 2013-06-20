3f8191a1b5ff3b5c550340ba0f7cc9f983cf07cb
Travis Loncar <loncar.travis@gmail.com>
2013-06-20 01:20:18 -0400

    Add exifr gem for photo metadata.

--- moment/db/migrate/20130620045257_create_users.rb
+++ moment/db/migrate/20130620045257_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :email
+      t.string :f_name
+      t.string :l_name
+      t.string :password_digest
+      t.text :bio
+      t.attachment :avatar
 
       t.timestamps
     end
--- moment/db/migrate/20130620045310_create_uploads.rb
+++ moment/db/migrate/20130620045310_create_uploads.rb
@@ -1,6 +1,8 @@
 class CreateUploads < ActiveRecord::Migration
   def change
     create_table :uploads do |t|
+      t.attachment :image
+
 
       t.timestamps
     end
